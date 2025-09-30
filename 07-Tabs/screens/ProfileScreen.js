aa04a2f97b00ab628bf319422465581ac90b5239
matheus2306 <math2306.r@gmail.com>
2025-09-30 14:29:27 -0300

    feat: enhance Profile and Settings screens with layout and styling improvements

--- 07-Tabs/screens/ProfileScreen.js
+++ 07-Tabs/screens/ProfileScreen.js
@@ -1,7 +1,18 @@
-import { Text } from "react-native";
+import { useState } from "react";
+import { SafeAreaView, StyleSheet, Text } from "react-native";
 
 export default function ProfileSccreen() {
-    return(
-        <Text>Profile</Text>
-    );
+  const [userId] = useState("111111");
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>👤 Tela de Perfil</Text>
+      <Text style={styles.text}>Seu Id único: {userId}</Text>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  text: { fontSize: 20, fontWeight: "bold", marginBottom: 10 },
+});
--- 07-Tabs/screens/SettingsScreens.js
+++ 07-Tabs/screens/SettingsScreens.js
@@ -1,7 +1,13 @@
-import { Text } from "react-native";
+import { SafeAreaView, StyleSheet, Text } from "react-native";
 
 export default function settingsScreens() {
-    return(
-        <Text>settings</Text>
-    );
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>⚙️ ajuste suas configurações aqui</Text>
+    </SafeAreaView>
+  );
 }
+const styles = StyleSheet.create({
+    container:{ flex: 1, justifyContent:"center", alignItems:"center"},
+    text:{fontWeight:"bold", fontSize:24,}
+});
